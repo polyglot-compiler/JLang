ad3aa5bd113aa973643d49d96e42e4797ab3d78c
Daniel Donenfeld <dbd64@users.noreply.github.com>
2016-10-17 01:37:06 -0400

    Added test cases for while and for loops

---
+++ tests/isolated/basic/WhileLoop.java
@@ -0,0 +1,11 @@
+import placeholder.Print;
+
+public class WhileLoop {
+    public static void main(String[] args){
+        int i = 0;
+        while(i<10){
+            i+=1;
+            Print.println(-i);
+        }
+    }
+}
