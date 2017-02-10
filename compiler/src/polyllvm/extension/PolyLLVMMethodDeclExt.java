ed8e5abd3008791368b57e620515e2b0a5d79e63
Matthew Gharrity <mjg355@cornell.edu>
2017-02-09 19:56:15 -0500

    Remove mistaken TODO

--- compiler/src/polyllvm/extension/PolyLLVMMethodDeclExt.java
+++ compiler/src/polyllvm/extension/PolyLLVMMethodDeclExt.java
@@ -2,7 +2,6 @@ package polyllvm.extension;
 
 import polyglot.util.SerialVersionUID;
 
-// TODO: Delete.
 public class PolyLLVMMethodDeclExt extends PolyLLVMProcedureDeclExt {
     private static final long serialVersionUID = SerialVersionUID.generate();
 }
