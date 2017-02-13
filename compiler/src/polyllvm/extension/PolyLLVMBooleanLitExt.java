f3cce558ba65924664dd9a91998a0d632772f1ae
Matthew Gharrity <mjg355@cornell.edu>
2017-02-12 23:07:31 -0500

    Add minor translations; remove some dead code

--- compiler/src/polyllvm/extension/PolyLLVMBooleanLitExt.java
+++ compiler/src/polyllvm/extension/PolyLLVMBooleanLitExt.java
@@ -1,12 +1,9 @@
 package polyllvm.extension;
 
-import org.bytedeco.javacpp.LLVM;
 import polyglot.ast.BooleanLit;
 import polyglot.ast.Node;
 import polyglot.util.SerialVersionUID;
 import polyllvm.ast.PolyLLVMExt;
-import polyllvm.ast.PolyLLVMNodeFactory;
-import polyllvm.ast.PseudoLLVM.Expressions.LLVMLabel;
 import polyllvm.visit.PseudoLLVMTranslator;
 
 import static org.bytedeco.javacpp.LLVM.*;
@@ -17,27 +14,17 @@ public class PolyLLVMBooleanLitExt extends PolyLLVMExt {
     @Override
     public Node translatePseudoLLVM(PseudoLLVMTranslator v) {
         BooleanLit n = (BooleanLit) node();
-        LLVMValueRef val = LLVMConstInt(LLVMInt1Type(), n.value() ? 1 : 0 ,0);
+        LLVMValueRef val = LLVMConstInt(LLVMInt1Type(), n.value() ? 1 : 0, /* sign-extend */ 0);
         v.addTranslation(node(), val);
         return super.translatePseudoLLVM(v);
     }
 
     @Override
-    public Node translatePseudoLLVMConditional(PseudoLLVMTranslator v,
-            LLVMLabel trueLabel, LLVMLabel falseLabel) {
+    public void translateLLVMConditional(PseudoLLVMTranslator v,
+                                         LLVMBasicBlockRef trueBlock,
+                                         LLVMBasicBlockRef falseBlock) {
         BooleanLit n = (BooleanLit) node();
-        PolyLLVMNodeFactory nf = v.nodeFactory();
         if (n.value()) {
-            return nf.LLVMBr(trueLabel);
-        }
-        else {
-            return nf.LLVMBr(falseLabel);
-        }
-    }
-
-    @Override
-    public void translateLLVMConditional(PseudoLLVMTranslator v, LLVM.LLVMBasicBlockRef trueBlock, LLVM.LLVMBasicBlockRef falseBlock) {
-        if (((BooleanLit) node()).value()) {
             LLVMBuildBr(v.builder, trueBlock);
         } else {
             LLVMBuildBr(v.builder, falseBlock);
