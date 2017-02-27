6c1ec40eccfb005ceef7cfba66452194623b1475
Daniel Donenfeld <dbd64@users.noreply.github.com>
2017-02-27 18:58:58 -0500

    Update exception.cpp so runtime compiles

--- runtime/jni/exception.cpp
+++ runtime/jni/exception.cpp
@@ -1,66 +1,23 @@
 #include <stdexcept>
+#include <cstring>
 
+#include <unwind.h>
 #include <inttypes.h>
-
-extern "C" {
-
-  typedef enum {
-    _URC_NO_REASON = 0,
-    _URC_FOREIGN_EXCEPTION_CAUGHT = 1,
-    _URC_FATAL_PHASE2_ERROR = 2,
-    _URC_FATAL_PHASE1_ERROR = 3,
-    _URC_NORMAL_STOP = 4,
-    _URC_END_OF_STACK = 5,
-    _URC_HANDLER_FOUND = 6,
-    _URC_INSTALL_CONTEXT = 7,
-    _URC_CONTINUE_UNWIND = 8
-  } _Unwind_Reason_Code;
-
-  typedef enum {
-    _UA_SEARCH_PHASE = 1,
-    _UA_CLEANUP_PHASE = 2,
-    _UA_HANDLER_FRAME = 4,
-    _UA_FORCE_UNWIND = 8,
-    _UA_END_OF_STACK = 16
-  } _Unwind_Action;
-
-  struct _Unwind_Exception;
-
-  typedef void (*_Unwind_Exception_Cleanup_Fn) (_Unwind_Reason_Code,
-                                                struct _Unwind_Exception *);
-
-  struct _Unwind_Exception {
-    uint64_t exception_class;
-    _Unwind_Exception_Cleanup_Fn exception_cleanup;
-
-    uintptr_t private_1;
-    uintptr_t private_2;
-
-    // @@@ The IA-64 ABI says that this structure must be double-word aligned.
-    //  Taking that literally does not make much sense generically.  Instead
-    //  we provide the maximum alignment required by any type for the machine.
-  } __attribute__((__aligned__));
-
-  struct _Unwind_Context;
-  typedef struct _Unwind_Context *_Unwind_Context_t;
-
-  extern const uint8_t *_Unwind_GetLanguageSpecificData (_Unwind_Context_t c);
-  extern uintptr_t _Unwind_GetGR (_Unwind_Context_t c, int i);
-  extern void _Unwind_SetGR (_Unwind_Context_t c, int i, uintptr_t n);
-  extern void _Unwind_SetIP (_Unwind_Context_t, uintptr_t new_value);
-  extern uintptr_t _Unwind_GetIP (_Unwind_Context_t context);
-  extern uintptr_t _Unwind_GetRegionStart (_Unwind_Context_t context);
-
-} // extern "C"
+#include <gc.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 typedef struct _Unwind_Exception UnwindException;
 
+extern "C" {
 
 struct JavaException_t {
   void* jexception;
 
   // Note: This is properly aligned in unwind.h
-  struct _Unwind_Exception unwindException;
+  UnwindException unwindException;
 };
 
 /// Generates our _Unwind_Exception class from a given character array.
@@ -99,10 +56,9 @@ void deleteJavaException(_Unwind_Reason_Code reason, UnwindException *expToDelet
 /// @param type type info type
 UnwindException *allocateJavaException(void* jexception) {
   size_t size = sizeof(JavaException_t);
-  void* allocate k.ug
-  JavaException_t *ret = (JavaException_t*) memset(malloc(size), 0, size);
+  JavaException_t *ret = (JavaException_t*) memset(GC_malloc(size), 0, size);
   (ret->jexception) = jexception;
-  (ret->unwindException).exception_class = genClass(polyLLVMExceptionClassChars);
+  (ret->unwindException).exception_class = genClass(polyLLVMExceptionClassChars, 8);
   (ret->unwindException).exception_cleanup = deleteJavaException;
 
   return(&(ret->unwindException));
@@ -110,7 +66,7 @@ UnwindException *allocateJavaException(void* jexception) {
 
 void throwJavaException(UnwindException* exception){
   _Unwind_RaiseException(exception);
-  std::abort();
+  abort();
 }
 
 /// Read a uleb128 encoded value and advance pointer
@@ -162,5 +118,7 @@ static uintptr_t readSLEB128(const uint8_t **data) {
   }
 
   return result;
+}
+
 }
 
