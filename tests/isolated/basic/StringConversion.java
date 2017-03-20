fbf6c1a158b0ff6a917883bd6c546801106437c4
Daniel Donenfeld <dbd64@users.noreply.github.com>
2017-03-20 12:04:07 -0400

    Update StringConversion to have correct package

--- tests/isolated/basic/StringConversion.java
+++ tests/isolated/basic/StringConversion.java
@@ -1,6 +1,5 @@
-/**
- * Created by Daniel on 3/13/17.
- */
+package basic;
+
 public class StringConversion {
     public static void main(String[] args) {
         System.out.println("Creating...\n" + new StringConversion() +"\nDone!");
