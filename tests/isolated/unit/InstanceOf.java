5436eca21439df9ee443e59b191236ecca913391
Matthew Gharrity <mjg355@cornell.edu>
2017-04-07 03:56:17 -0400

    Move and improve InstanceOf test

---
+++ tests/isolated/unit/InstanceOf.java
@@ -0,0 +1,30 @@
+package unit;
+
+class InstanceOf implements IA {
+
+    public static void main(String[] args) {
+        f(new Subclass());
+    }
+
+    public static void f(Object o) {
+        System.out.println(o instanceof Subclass);
+        System.out.println(o instanceof InstanceOf);
+        System.out.println(o instanceof Object);
+        System.out.println(o instanceof String);
+        System.out.println(o instanceof IA);
+        System.out.println(o instanceof IB);
+        System.out.println(o instanceof IC);
+        System.out.println(o instanceof ID);
+        System.out.println(o instanceof IE);
+        System.out.println(o instanceof IF);
+    }
+}
+
+class Subclass extends InstanceOf implements IE {}
+
+interface IA {}
+interface IB {}
+interface IC {}
+interface ID extends IA {}
+interface IE extends IB {}
+interface IF extends IC {}
