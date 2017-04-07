e579feb33fd7ab019cb8f9f8de6cc1dabb827edf
Matthew Gharrity <mjg355@cornell.edu>
2017-04-07 06:22:32 -0400

    Move labeled while test to loop test

--- tests/isolated/unit/Loop.java
+++ tests/isolated/unit/Loop.java
@@ -4,6 +4,8 @@ public class Loop {
     public static void main(String[] args) {
         int n = 10;
         for (int i = 0; i < n; ++i) {
+            if (i % 2 == 1)
+                continue;
             if (i > 0)
                 System.out.print(" ");
             System.out.print(i);
@@ -16,6 +18,9 @@ public class Loop {
                 System.out.print(" ");
             System.out.print(i);
             ++i;
+            if (i > n/2) {
+                break;
+            }
         }
         System.out.println();
 
@@ -32,5 +37,28 @@ public class Loop {
             ++i;
         } while (i < n);
         System.out.println();
+
+        boolean x = true;
+        outer: while (x) {
+            System.out.println("inside outer");
+            inner: while (x) {
+                System.out.println("inside inner");
+                break outer;
+            }
+            System.out.println("no");
+        }
+        System.out.println("yes");
+
+        outer2: for (int j = 0; j < n; ++j) {
+            System.out.println("inside outer2");
+            inner2: for (int k = j + 1; k < n; ++k) {
+                System.out.println("inside inner2");
+                if (k % 2 == 0) {
+                    continue outer2;
+                }
+            }
+            System.out.println("no");
+        }
+        System.out.println("yes");
     }
 }
