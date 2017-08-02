9b547e01d536f1f03bc28d423d338e327b60e764
Matthew Gharrity <mjg355@cornell.edu>
2017-08-01 22:16:00 -0700

    Fix: instanceof null should return false

--- runtime/jni/reflect.cpp
+++ runtime/jni/reflect.cpp
@@ -5,6 +5,8 @@
 extern "C" {
 
 bool instanceof(jobject* obj, void* compare_type_id) {
+    if (obj == nullptr)
+        return false;
     type_info* type_info = obj->dv->type_info;
     for (int32_t i = 0, end = type_info->size; i < end; ++i)
         if (type_info->super_type_ids[i] == compare_type_id)
