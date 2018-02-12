a7928e2ed8617242dbd9269bfb6a1e49429c1b56
Matthew Gharrity <gharrma@gmail.com>
2018-02-12 13:11:40 -0500

    Add signal handler to report traps before aborting

--- runtime/jni/main.cpp
+++ runtime/jni/main.cpp
@@ -1,5 +1,7 @@
-#include <string.h>
-#include <stdio.h>
+#include <csignal>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include "types.h"
 #include "gc.h"
 
@@ -12,8 +14,29 @@ jstring* Java_support_Factory_createString___3B(jarray* bytes);
 
 } // extern "C"
 
+static void sigaction(int sig, siginfo_t* info, void* ucontext) {
+    fprintf(stderr, "Aborting due to signal: %s\n", strsignal(sig));
+    if (sig == SIGSEGV)
+        fprintf(stderr, "This probably indicates a null pointer exception.\n");
+    if (sig == SIGFPE)
+        fprintf(stderr, "This indicates an arithmetic exception "
+                        "(e.g., divide by zero).\n");
+    fflush(stderr);
+    abort();
+}
+
 int main(int argc, char** argv) {
-    GC_INIT(); //Initialize the garbage collector
+    // Initialize the garbage collector.
+    GC_INIT();
+
+    // Set up signal handling to report (for example) null pointer exceptions.
+    struct sigaction sa;
+    sa.sa_sigaction = sigaction;
+    sa.sa_mask = 0;
+    sa.sa_flags =  SA_SIGINFO;
+    sigaction(SIGSEGV, &sa, 0);
+    sigaction(SIGBUS, &sa, 0);
+    sigaction(SIGFPE, &sa, 0);
 
     // Ignore the 0th argument, which is the name of the program.
     --argc, ++argv;
