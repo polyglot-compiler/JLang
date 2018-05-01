c50321374c6a2ee3a7ec2a3973d881e14b35701f
Matthew Gharrity <gharrma@gmail.com>
2018-05-01 12:37:23 -0400

    Workaround for file manager issue
    The default file manager will try to load classes by reflection
    if they are not found through the normal class path (apparently
    as a workaround for issues related to JDK 9). This doesn't always work
    for PolyLLVM, possibly related to the fact that we're running
    PolyLLVM with JDK 8 yet using it to compile JDK 7.
    
    The easy workaround for now is to override
    ExtFileManager#getJavaFileForInput.

--- compiler/src/polyllvm/PolyLLVMExtensionInfo.java
+++ compiler/src/polyllvm/PolyLLVMExtensionInfo.java
@@ -4,14 +4,21 @@ import polyglot.ast.NodeFactory;
 import polyglot.ext.jl5.ast.JL5ExtFactory_c;
 import polyglot.ext.jl7.JL7ExtensionInfo;
 import polyglot.ext.jl7.ast.JL7ExtFactory_c;
+import polyglot.filemanager.ExtFileManager;
+import polyglot.filemanager.FileManager;
 import polyglot.frontend.Scheduler;
 import polyglot.main.Options;
 import polyglot.main.Version;
 import polyglot.types.TypeSystem;
+import polyglot.util.StringUtil;
 import polyllvm.ast.PolyLLVMExtFactory_c;
 import polyllvm.ast.PolyLLVMLang_c;
 import polyllvm.ast.PolyLLVMNodeFactory_c;
 import polyllvm.types.PolyLLVMTypeSystem_c;
+
+import javax.tools.JavaFileObject;
+import javax.tools.JavaFileObject.Kind;
+import java.io.IOException;
 
 /**
  * Extension information for polyllvm extension.
@@ -65,5 +72,23 @@ public class PolyLLVMExtensionInfo extends JL7ExtensionInfo {
     @Override
     public Version version() {
         return new polyllvm.Version();
+    }
+
+    @Override
+    public FileManager createFileManager() {
+        return new ExtFileManager(this) {
+            @Override
+            public JavaFileObject getJavaFileForInput(
+                    Location location, String className, Kind kind) throws IOException {
+                // Explicitly avoid using reflection---as
+                // super.getJavaFileForInput sometimes does---because
+                // Polyglot runs with JDK 8, but compiles Java 7 code.
+                // XXX This is a workaround; there may be a cleaner solution.
+                String pkg = StringUtil.getPackageComponent(className);
+                String name = StringUtil.getShortNameComponent(className);
+                String relativeName = name + kind.extension;
+                return (JavaFileObject) getFileForInput(location, pkg, relativeName);
+            }
+        };
     }
 }
