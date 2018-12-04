340064afbd1d79abbb5207b758835fa521b4ede3
dz333 <dzag@cs.cornell.edu>
2018-12-04 18:33:57 -0500

    Added compile time intialization of static fields whose values are constants

--- tests/isolated/FinalField.java
+++ tests/isolated/FinalField.java
@@ -2,10 +2,19 @@ public class FinalField {
 
     static final int b = calculate();
     static final int a = 3;
+    static int d = calculate2();
+    static final boolean c = true;
     final int x;
     final int y;
+    static final int i3 = testString();
+    static final String message = (new FinalField()).toString();
 
+    static final String m = "this is a message";
+
+    private static int calculate2() { return c ? a : b; }
     private static int calculate() { return a; }
+
+    private static int testString() { return m.length(); }
 
     public FinalField(){
 
@@ -15,18 +24,29 @@ public class FinalField {
         System.out.println(this);
         y = 4;
         System.out.println(this);
-        //PUT Ds here
 
     }
 
     @Override
     public String toString() {
-        return "(" /*+ x + ", " + y */ + ")";
+        return "(" + x + ", " + y  + ")";
     }
 
     public static void main(String[] args) {
+	System.out.println("Printing non-static message");
         System.out.println(new FinalField().toString());
+	System.out.println("---------------------------");
 	System.out.println(a == b);
+	System.out.println(a == d);
+	System.out.println(m);
+	System.out.println(m == "this is a message");
+	System.out.println(m == "this is a message".intern());
+    }
+
+    static {
+	System.out.println("Printing static message");
+	System.out.println(message);
+	System.out.println("-----------------------");
     }
 
 }
