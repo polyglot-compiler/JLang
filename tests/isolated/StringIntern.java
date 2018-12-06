2a6afab79c8c0043079403140acadde8abf07066
Drew Zagieboylo <drew@dhcp-rhodes-971.eduroam.cornell.edu>
2018-12-06 11:45:11 -0500

    minor change to string test to check for multiple changed binops on constant strings

--- tests/isolated/StringIntern.java
+++ tests/isolated/StringIntern.java
@@ -3,7 +3,7 @@ class StringIntern {
     public static void main(String[] args) {
         System.out.println("hello" == "hello");
         // this should succeed once we add string constant folding
-        System.out.println("hello" == "hel"+"lo");
+        System.out.println("hello" == "hel"+"l" + "o");
         System.out.println("hello" == ("hel"+"lo").intern());
         System.out.println(("he"+"llo").intern() == ("hel"+"lo").intern());
         String he = "he";
