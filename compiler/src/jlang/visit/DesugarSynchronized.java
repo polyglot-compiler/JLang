b6de5104bb2d218369f753b1dfbfdfe8780d5487
Yiteng Guo <guoyiteng@gmail.com>
2019-10-24 23:10:36 -0400

    emit synchronized method call in the caller site

--- compiler/src/jlang/visit/DesugarSynchronized.java
+++ compiler/src/jlang/visit/DesugarSynchronized.java
@@ -30,54 +30,31 @@ public class DesugarSynchronized extends DesugarVisitor {
     protected Node leaveDesugar(Node n) throws SemanticException {
 
         if (n instanceof Synchronized) {
-            return desugarSynchronizedNode((Synchronized) n);
-        } else if (n instanceof MethodDecl) {
-            MethodDecl node = (MethodDecl) n;
-            if (node.flags().isSynchronized() && node.body() != null) {
-                // TODO: how to handle public synchronized native void foo()
-                // Remove && node.body() != null after figure out how to handle it
-                Block body = node.body();
-                Expr synchronizedObj;
-                if (node.flags().isStatic()) {
-                    synchronizedObj = tnf.ClassLit(body.position(), currentClass);
-                } else {
-                    synchronizedObj = tnf.UnqualifiedThis(body.position(), currentClass);
-                }
-                Synchronized synchronizedBody = nf.Synchronized(body.position(), synchronizedObj, body);
-                return node.body(desugarSynchronizedNode(synchronizedBody));
-            }
+            // Desugar
+            //  synchronized(o) {...}
+            // To
+            //  {
+            //      Object temp = o;
+            //      try { MonitorEnter(o); ... }
+            //      finally { MonitorExit(o); }
+            //  }
+            Synchronized node = (Synchronized) n;
+            Position pos = node.position();
+
+            LocalDecl declObj = tnf.TempSSA("syncObj", node.expr());
+            Local obj = tnf.Local(pos, declObj);
+
+            SynchronizedEnter syncEnter = nf.SynchronizedEnter(pos, copy(obj));
+            SynchronizedExit syncExit = nf.SynchronizedExit(pos, copy(obj));
+
+            Block tryBlock = nf.Block(pos, syncEnter, node.body());
+            Block finallyBlock = nf.Block(pos, syncExit);
+
+            Try tryFinally = nf.Try(pos, tryBlock, Collections.emptyList(), finallyBlock);
+
+            return nf.Block(pos, declObj, tryFinally);
         }
 
         return n;
-    }
-
-    /**
-     * Desugar
-     * synchronized(o) {...}
-     * To
-     * {
-     * Object temp = o;
-     * try { synchronized_enter(o); ... }
-     * finally {synchronized_exit(o); }
-     * }
-     *
-     * @param node the desugared input
-     * @return the desugared output
-     */
-    private Block desugarSynchronizedNode(Synchronized node) {
-        Position pos = node.position();
-
-        LocalDecl declObj = tnf.TempSSA("syncObj", node.expr());
-        Local obj = tnf.Local(pos, declObj);
-
-        SynchronizedEnter syncEnter = nf.SynchronizedEnter(pos, copy(obj));
-        SynchronizedExit syncExit = nf.SynchronizedExit(pos, copy(obj));
-
-        Block tryBlock = nf.Block(pos, syncEnter, node.body());
-        Block finallyBlock = nf.Block(pos, syncExit);
-
-        Try tryFinally = nf.Try(pos, tryBlock, Collections.emptyList(), finallyBlock);
-
-        return nf.Block(pos, declObj, tryFinally);
     }
 }
