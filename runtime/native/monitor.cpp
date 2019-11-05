b6845b06f99ac80578e888b42aa3e422fe1a050b
Yiteng Guo <guoyiteng@gmail.com>
2019-11-04 21:54:30 -0500

    add TODO in monitor.cpp

--- runtime/native/monitor.cpp
+++ runtime/native/monitor.cpp
@@ -15,6 +15,7 @@ void Monitor::enter(jobject obj) { syncVars.push_back(obj); }
 void Monitor::exit(jobject obj) {
     jobject enter = syncVars.back();
     syncVars.pop_back();
+    // TODO: implement the actual monitor enter and exit.
     // A simple test to check the correctness of the inplementation of
     // synchronized keyword.
     if (enter != obj) {
